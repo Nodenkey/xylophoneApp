7e07c794a90c092d8964f5bb2e99d0bcf2bb35e6
Nodenkey <Nodenkey@gmail.com>
2019-12-26 18:37:38 +0000

    "added tiles"

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\GTL-LP18\Desktop\nii\xylophoneApp\xylophone"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/user/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/user/AndroidStudioProjects/xylophoneApp"
+export "FLUTTER_TARGET=/Users/user/AndroidStudioProjects/xylophoneApp/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\src\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/user/Developer/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,32 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  Widget tile(int soundNum, MaterialColor color) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {},
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              tile(1, Colors.red),
+              tile(2, Colors.orange),
+              tile(3, Colors.yellow),
+              tile(4, Colors.green),
+              tile(5, Colors.blue),
+              tile(6, Colors.indigo),
+              tile(7, Colors.deepPurple)
+            ],
+          ),
         ),
       ),
     );
